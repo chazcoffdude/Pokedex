8f9ffe6a6b39d73359d2a4fc7ed8d8f627ae1208
Hanad <69435603+Hshipp267@users.noreply.github.com>
2021-04-28 18:13:47 -0700

    fix plz

--- JS/pokedex.js
+++ JS/pokedex.js
@@ -1,4 +1,4 @@
-
+//charmander
 let charinput1 = 
 {
   "abilities": [
@@ -73,7 +73,7 @@ let charinput1 =
 
 let char = JSON.stringify(charinput1);
 
-char = JSON.parse
+var char2 = JSON.parse(char);
 
 
 function varget(input){
@@ -100,5 +100,119 @@ function varget(input){
 
 
 }
-document.getElementById("testvar").innerHTML = "Dogs can do flips"
+document.getElementById('name').innerHTML = char2.name;
+document.getElementById('type').innerHTML = char2.types[0].type.name;
+document.getElementById('abilite').innerHTML = char2.abilities[0].name;
+document.getElementById('abilites').innerHTML = char2.abilities[1].name;
+document.getElementById('baseXP').innerHTML = char2.base_experience;
+document.getElementById('height').innerHTML = char2.height;
+document.getElementById('weight').innerHTML = char2.weight;
+document.getElementById('hp').innerHTML = char2.stats[0].base_stat;
+document.getElementById('atk').innerHTML = char2.stats[1].base_stat;
+document.getElementById('def').innerHTML = char2.stats[2].base_stat;
+document.getElementById('spatk').innerHTML = char2.stats[3].base_stat;
+document.getElementById('spdef').innerHTML = char2.stats[4].base_stat;
+document.getElementById('sped').innerHTML = char2.stats[5].base_stat;
+document.getElementById('Bhp').innerHTML = char2.stats[0].effort;
+document.getElementById('Batk').innerHTML = char2.stats[1].effort;
+document.getElementById('Bdef').innerHTML = char2.stats[2].effort;
+document.getElementById('Bspatk').innerHTML = char2.stats[3].effort;
+document.getElementById('Bspdef').innerHTML = char2.stats[4].effort;
+document.getElementById('Bsped').innerHTML = char2.stats[5].effort;
 varget(charinput1);
+
+//charmelon
+let inventory = 
+{
+  "abilities": [
+    {
+      "name": "blaze"
+    },
+    {
+      "name": "solar-power"
+    }
+  ],
+  "base_experience": 142,
+  "height": 11,
+  "id": 5,
+  "is_default": true,
+  "name": "charmeleon",
+  "order": 6,
+  "stats": [
+    {
+      "base_stat": 58,
+      "effort": 0,
+      "stat": {
+        "name": "hp"
+      }
+    },
+    {
+      "base_stat": 64,
+      "effort": 0,
+      "stat": {
+        "name": "attack"
+      }
+    },
+    {
+      "base_stat": 58,
+      "effort": 0,
+      "stat": {
+        "name": "defense"
+      }
+    },
+    {
+      "base_stat": 80,
+      "effort": 1,
+      "stat": {
+        "name": "special-attack"
+      }
+    },
+    {
+      "base_stat": 65,
+      "effort": 0,
+      "stat": {
+        "name": "special-defense"
+      }
+    },
+    {
+      "base_stat": 80,
+      "effort": 1,
+      "stat": {
+        "name": "speed"
+      }
+    }
+  ],
+  "types": [
+    {
+      "slot": 1,
+      "type": {
+        "name": "fire"
+      }
+    }
+  ],
+  "weight": 190
+}
+
+let melon = JSON.stringify(inventory);
+
+var pokeon = JSON.parse(melon);
+document.getElementById("name2").innerHTML = pokeon.name;
+document.getElementById('type2').innerHTML = pokeon.types[0].type.name;
+document.getElementById('abilite2').innerHTML = pokeon.abilities[0].name;
+document.getElementById('abilites2').innerHTML = pokeon.abilities[1].name;
+document.getElementById('baseXP2').innerHTML = pokeon.base_experience;
+document.getElementById('height2').innerHTML = pokeon.height;
+document.getElementById('weight2').innerHTML = pokeon.weight;
+document.getElementById('hp2').innerHTML = pokeon.stats[0].base_stat;
+document.getElementById('atk2').innerHTML = pokeon.stats[1].base_stat;
+document.getElementById('def2').innerHTML = pokeon.stats[2].base_stat;
+document.getElementById('spatk2').innerHTML = pokeon.stats[3].base_stat;
+document.getElementById('spdef2').innerHTML = pokeon.stats[4].base_stat;
+document.getElementById('sped2').innerHTML = pokeon.stats[5].base_stat;
+document.getElementById('Bhp2').innerHTML = pokeon.stats[0].effort;
+document.getElementById('Batk2').innerHTML = pokeon.stats[1].effort;
+document.getElementById('Bdef2').innerHTML = pokeon.stats[2].effort;
+document.getElementById('Bspatk2').innerHTML = pokeon.stats[3].effort;
+document.getElementById('Bspdef2').innerHTML = pokeon.stats[4].effort;
+document.getElementById('Bsped2').innerHTML = pokeon.stats[5].effort;
+varget(inventory);
